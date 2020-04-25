6ea63c85b0dc4f58f4c0fce7c8efbb400f63af31
jggruber247 <jggruber247@gmail.com>
2020-04-25 11:50:15 -0400

    Progress

--- Python Programs/numberGuess.py
+++ Python Programs/numberGuess.py
@@ -1,5 +1,24 @@
+#Garrett Gruber
+#April 25, 2020
+#A simple guessing game Python program to refresh my skills with the language.
+
 import random
 n = random.randint(1,100)
+#print(n)
 print("Guess a number between 1 and 100 (inclusively)")
 guess = int(input("Your guess: "))
-print(guess)
+#print(guess)
+if n == guess:
+    print("Your guess is CORRECT.")
+else:
+    while n != guess:
+        if guess < n:
+            print("You guessed TOO LOW.")
+            print("Guess another number between 1 and 100 (inclusively)")
+            guess = int(input("Your guess: "))
+        elif guess > n:
+            print("You guessed TOO HIGH.")
+            print("Guess another number between 1 and 100 (inclusively)")
+            guess = int(input("Your guess: "))
+    print("Your guess is CORRECT.")
+    
